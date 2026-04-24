969cac3e629a750449f176b0672e4a84948381a3
Nikkolas <jn147860@gmail.com>
2026-04-23 22:51:36 -0500

    Añadí la estructura y hice la primera vista que es el formulario de inicio de sesión

---
+++ android/app/src/main/kotlin/com/example/elegant_cut_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.elegant_cut_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+// Puente de la pantalla de login de la primera pantalla uwu
+import 'package:elegant_cut_mobile/src/pages/login_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Elegant Cut',
+      //aca definimos el fondo
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color(0xFFD48B41),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFFD48B41),
+          surface: Color(0xFF121212),
+        ),
+        fontFamily: 'Roboto',
+      ),
+
+      home: const LoginPage(),
+    );
+  }
+}
