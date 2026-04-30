5d7e568e760a4974343f4b7ada95bb5ad096a991
EdwarStick <edwarsgl0318@gmail.com>
2026-04-30 15:45:56 -0500

    Elimino espacios

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -2,7 +2,7 @@ class ApiConstants {
   // En emulador Android, 10.0.2.2 apunta al localhost de tu PC
   // En dispositivo físico, usa tu IP local (ej: 192.168.1.50)
   static const String baseUrl = 'http://10.0.2.2:3001/api';
-  
+
   // Endpoints
   static const String login = '/auth/login';
   static const String register = '/auth/register';
